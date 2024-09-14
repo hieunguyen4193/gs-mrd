b449ff3e16e6dc8ed5649979a9338faaede21df9
hieunguyen4193 <work@hieunguyen.de>
2024-09-14 23:01:56 +0200

    update

---
+++ helper_functions.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+import pathlib
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn import metrics
+from sklearn.metrics import RocCurveDisplay
+from sklearn.decomposition import NMF
+import warnings
+warnings.filterwarnings("ignore")
+import os
+import ot
+import pickle
+import argparse
+
+##### Helper functions for OPTIMAL TRANSPORT DISTANCE
+
+def calculate_barycenter(A, n = 301, show_plot=False, M = None):
+    n_distributions = A.shape[1]
+
+    # loss matrix + normalization
+    if M is None:
+        M = ot.utils.dist0(n)
+        M /= M.max()
+
+    weights = [1/n_distributions for item in range(n_distributions)]
+    x = np.arange(n, dtype=np.float64)
+    # l2bary
+    bary_l2 = A.dot(weights)
+
+    # wasserstein
+    reg = 1e-3
+    if show_plot:
+        f, (ax1, ax2) = plt.subplots(2, 1, tight_layout=True, num=1)
+        ax1.plot(x, A, color="black")
+        ax1.set_title('Distributions')
+
+        ax2.plot(x, bary_l2, 'r', label='l2')
+        ax2.set_title('Barycenters')
+
+        plt.legend()
+        plt.show()
+    return bary_l2
