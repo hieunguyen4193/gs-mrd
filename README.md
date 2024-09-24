3499e80f452a62446d2f4edeaacded2cc24dd540
hieunguyen4193 <work@hieunguyen.de>
2024-09-24 21:56:47 +0200

    update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# MRD projects
+# Pipeline
 
+- Run `metadata_split_for_training_and_testing.ipynb` to generate the `release_metadata`. 
+
+- Run `01_generate_baseModels_for_a_release.ipynb` to generate model files for the release, this corresponds to the `release_metadata` generated in the previous step.
--- helper_functions.py
+++ helper_functions.py
@@ -41,3 +41,13 @@ def calculate_barycenter(A, n = 301, show_plot=False, M = None):
         plt.legend()
         plt.show()
     return bary_l2
+
+def calculate_ot_distance_to_healthy_nuc(sample, bary_l2, inputdf, n):
+    x = np.arange(n, dtype=np.float64)
+    M = ot.dist(x.reshape((n, 1)), x.reshape((n, 1)), 'euclidean')
+    M /= M.max() * 0.1
+    a = inputdf[sample].values
+    a = np.array(a)
+    b = bary_l2
+    d_emd = ot.emd2(a, b, M)  # direct computation of OT loss
+    return d_emd
